963a5dd93501c6585dfb22764490cc284c109193
alfabeat <williamunderh@gmail.com>
2025-03-06 01:16:37 +0000

    second

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,8 +11,12 @@ message: 'Welcome to my REST API!'
 });
 // Import controllers here
 import * as userController from "../Controllers/UnitsControllers.js"
+import verifyToken from '../auth/auth.js'
 // define routes here
 router.route('/login')
     .post(userController.login);
+
+    router.route('/admin')
+    .get([verifyToken, userController.admin]);
 // Export API routes. As it is the only export, we make it the default.
 export default router;
