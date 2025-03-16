4c546ac91330dfa3e874b03966da870a3d6143c6
alfabeat <86717951+alfabeat@users.noreply.github.com>
2025-03-16 04:39:26 +0000

    Update auth.config.js

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,4 +1,1 @@
-export default {
-    secret:"simulator-authorisation-key",
-    mongo:"mongodb+srv://williamunderh:pacwill01@clusterapimovie0.4l2lb.mongodb.net/?retryWrites=true&w=majority&appName=Clusterapimovie0"
-    };
+
